d17a7ae4051971d1752d0e16e63613206cf9c669
Rainer Deißler <deisslertelekom@gmail.com>
2021-06-09 11:33:49 +0200

    Projekt WebServer angelegt

---
+++ src/main/java/de/telekom/sea3/webserver/WebServer.java
@@ -0,0 +1,16 @@
+
+package de.telekom.sea3.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**    Main   **/
+
+@SpringBootApplication
+public class WebServer /* implements SpringBootApplication */ {
+	public static void main(String[] args) {
+		SpringApplication.run(WebServer.class, args);
+
+	}
+
+}
