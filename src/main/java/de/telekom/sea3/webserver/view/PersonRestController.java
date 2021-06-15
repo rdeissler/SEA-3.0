6921a2e437690d87f1c66c9eceeef61c1663ad61
Rainer Deißler <deisslertelekom@gmail.com>
2021-06-15 13:06:06 +0200

    getSizeund getAllPersons in PersonRestController geändert

--- src/main/java/de/telekom/sea3/webserver/view/PersonRestController.java
+++ src/main/java/de/telekom/sea3/webserver/view/PersonRestController.java
@@ -1,64 +1,40 @@
 package de.telekom.sea3.webserver.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.telekom.sea3.webserver.model.Personen;
 import de.telekom.sea3.webserver.service.PersonService;
 
-@Controller
+@RestController
 public class PersonRestController { // View
 
-	
 	private PersonService personService;
 
 	@Autowired
 	public PersonRestController(PersonService personService) {
-		super(); 
-	//	this.getAllPersons();
-			this.personService = personService;
+		super();
+		// this.getAllPersons();
+		this.personService = personService;
 	}
 
-		
-		@GetMapping("/json/persons/all") /**  @see URL: <a href= "http//localhost:8080/json/persons/all">URL</a> Kommentar */
-		@ResponseBody 
-		public String getAllPersons() { 
-			Personen personen = personService.getAllPersons();
-	//		personen.toJson();
-			String string1 = "{\n"
-					+ "	\"personen\": [\n"
-					+ "		{\n"
-					+ "			\"vorname\": \"Paula\",\n"
-					+ "			\"nachname\": \"Meisen\",\n"
-					+ "			\"anrede\": \"Frau\"\n"
-					+ "		},\n"
-					+ "		{\n"
-					+ "			\"vorname\": \"Paul\",\n"
-					+ "			\"nachname\": \"Paulsen\",\n"
-					+ "			\"anrede\": \"Herr\"\n"
-					+ "		},\n"
-					+ "		{\n"
-					+ "			\"vorname\": \"Mike\",\n"
-					+ "			\"nachname\": \"Lienen\",\n"
-					+ "			\"anrede\": \"Herr\"\n"
-					+ "		}\n"
-					+ "	]\n"
-					+ "}";
-			return string1;
-		}
-		
-		
-		@GetMapping("/json/persons/size") /**  @see URL: <a href= "http//localhost:8080/json/persons/all">URL</a> Kommentar */
-		@ResponseBody 
-		public String getSize() { 
-			int size = personService.getSize();
-	//		personen.toJson();
-			String string1 = String.format("{\n"
-					+ "	\"size\": %d \n"
-					+ "}",size);
-			return string1;
-		}
 	
+	/**
+	 * @see URL: <a href= "http//localhost:8080/json/persons/all">URL</a> Kommentar
+	 */
+	@GetMapping("/json/persons/all")
+	public Personen getAllPersons() {
+		Personen personen = personService.getAllPersons();
+		return personen;
+		// personen.toJson();
+	}
+
+	@GetMapping("/json/persons/size")
+	public Integer getSize() {
+		int size = personService.getSize();
+		return size;
+	}
+
 }
